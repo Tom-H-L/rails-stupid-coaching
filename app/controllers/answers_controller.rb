b113b0b5c2ad2749afd621068534a3376d5dec8f
Tom Lautenbacher <tom@lautenbacher.biz>
2021-02-16 12:54:47 +0100

    50%

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,14 @@
 class AnswersController < ApplicationController
   def answer
+    # if your_message == (exit_trigger || exit_trigger.upcase)
+    #   puts "\nWas about time! Good bye."
+    #   exit
+    # elsif your_message.include? "?"
+    #   answer = "Silly question, get dressed and go to work!"
+    # else
+    #   answer = "I don't care, get dressed and go to work!"
+    # end
+    # answer
+    raise
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @answers = ["answer 1", "answer2"....]
+    if params[:question == "I am going to work"]
+    elsif params[:question].end_with?("?") #your_message.include? "?"
+      @answer = @answers[0] #"Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+      # raise
+  end
 end
